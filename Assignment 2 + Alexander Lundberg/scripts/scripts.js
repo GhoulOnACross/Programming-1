87c523b995fa7382dbe67528e6d3f9b607d71440
GhoulOnACross <alexander.lundberg@outlook.com>
2024-11-20 20:29:17 +0100

    Web upload

---
+++ Assignment 2 + Alexander Lundberg/scripts/scripts.js
@@ -0,0 +1,15 @@
+let array = ["Lilly", "Alrik", "Hanna", "Emelina", "Filip", "David", "Anna", "Sophie", "Chazz"];
+
+array.sort();
+
+if (true) {
+  console.log("yes");
+}
+
+let i = 0;
+
+while (i < a.length) {
+    console.log("loopar " + i);
+    document.body.innerHTML += "<br>" + a[i];
+    i ++
+}
