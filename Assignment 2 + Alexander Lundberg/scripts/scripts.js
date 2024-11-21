12802afd9c4d1dbcbb447f5cb8fb0ad0de471949
GhoulOnACross <alexander.lundberg@outlook.com>
2024-11-21 18:44:47 +0100

    Uppgift 2 klar

--- Assignment 2 + Alexander Lundberg/scripts/scripts.js
+++ Assignment 2 + Alexander Lundberg/scripts/scripts.js
@@ -1,15 +1,32 @@
-let array = ["Lilly", "Alrik", "Hanna", "Emelina", "Filip", "David", "Anna", "Sophie", "Chazz"];
+//Assignment 2a:
 
-array.sort();
-
-if (true) {
-  console.log("yes");
+{
+  for (let i = 0; i < 10; i++) {
+    if (i === 4) document.write("Q");
+    else if (i % 2 === 0) document.write("O");
+    else document.write("X");
+  }
 }
 
-let i = 0;
+document.writeln("<br></br>");
 
-while (i < a.length) {
-    console.log("loopar " + i);
-    document.body.innerHTML += "<br>" + a[i];
-    i ++
+//Assignment 2b:
+
+{
+  for (let i = 0; i < 20; i++) {
+    let randomSpace = Math.floor(Math.random() * 5) + 1;
+    let spaces = "&nbsp;".repeat(randomSpace);
+    document.write("O" + spaces);
+  }
 }
+
+//Assignment 2c:
+
+let cityNames = ["kiruna", "uppsala", "malmo", "gothenburg", "stockholm"];
+cityNames.forEach((word) => {
+  let capital = word.charAt(0).toUpperCase() + word.slice(1);
+  let letters = capital.split("");
+  letters.forEach((letter) => {
+    console.log(letter);
+  });
+});
